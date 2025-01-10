0ec16c8bd2941bceac54ef8e2e8c4aebecaae227
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2025-01-10 18:45:55 +0100

    fix some issues

--- Assets/Scripts/Enemy/EnemyFire.cs
+++ Assets/Scripts/Enemy/EnemyFire.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class EnemyFire : MonoBehaviour
 {
-    [SerializeField] private float timeToDestroy = 15f;
     [SerializeField] private ParticleSystem explosionPrefab;
     [SerializeField] private float speed = 15f;
 
-    private void Awake()
-    {
-        StartCoroutine(DestroyAfterTime());
-    }
-
-    private IEnumerator DestroyAfterTime()
-    {
-        yield return new WaitForSeconds(timeToDestroy);
-        Destroy(gameObject);
-    }
+    
 
     private void FixedUpdate()
     {
@@ -26,13 +16,11 @@ public class EnemyFire : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (explosionPrefab != null)
-        {
-            var explosion = Instantiate(explosionPrefab, other.contacts[0].point, Quaternion.identity);
-            explosion.gameObject.SetActive(true);
-            explosion.Play();
-            Destroy(explosion.gameObject, 2f);
-        }
+        var explosion = Instantiate(explosionPrefab, other.contacts[0].point, Quaternion.identity);
+        explosion.gameObject.SetActive(true);
+        explosion.Play();
+        Destroy(explosion.gameObject, 2f);
+
 
         Destroy(gameObject);
     }
